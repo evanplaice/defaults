203458fd85244ac212eeae574c0af76dd8684259
Evan Plaice <evanplaice@gmail.com>
2024-05-21 21:02:11 -0600

    Update
    Finish bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,40 +1,49 @@
 #!/bin/bash
 
-# install brew
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.bash_profile
-# eval "$(/opt/homebrew/bin/brew shellenv)"
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Prereqs
+softwareupdate --install-rosetta
 
 # GUI Applications
-# brew install --cask brave-browser
-# brew install --cask chromium
-# brew install --cask cloudapp
-# brew install --cask discord
-# brew install --cask keepassxc
-# brew install --cask gitkraken
-# brew install --cask google-chrome
-# brew install --cask iterm2
-# brew install --cask openemu
-# brew install --cask paragon-ntfs
-# brew install --cask plex-media-server
-# brew install --cask rectangle
-# brew install --cask shotcut
-# brew install --cask spotify
-# brew install --cask steam
-# brew install --cask sublime-text
-# brew install --cask transmission
-# brew install --cask vagrant
-# brew install --cask virtualbox
-# brew install --cask visual-studio-code
-# brew install --cask vlc
+brew install --cask brave-browser
+brew install --cask chromium
+xattr -cr /Applications/Chromium.app
+brew install --cask discord
+brew install --cask gitkraken
+brew install --cask google-chrome
+# brew install --cask keepassxc # replaced by Strongbox
+brew install --cask openemu
+brew install --cask paragon-ntfs
+brew install --cask plex-media-server
+brew install --cask rectangle
+brew install --cask shotcut
+brew install --cask slack
+brew install --cask spotify
+brew install --cask steam
+brew install --cask sublime-text
+brew install --cask transmission
+brew install --cask vagrant
+brew install --cask virtualbox
+brew install --cask visual-studio-code
+brew install --cask vlc
+brew install --cask warp
 
 # CLI Applications
-# brew install awscli
-# brew install docker
-# brew install docker-compose
-# brew install git
-# brew install -g node
-# brew install python@3.9
+brew install awscli
+brew install docker
+brew install docker-compose
+brew install git
+brew install -g node
+brew install python@3.10
+
+# NodeJS Globals
+npm i -g tree
 
 # Config
-# git config --global init.defaultBranch "main"
+git config --global user.name "Evan Plaice"
+git config --global user.email evanplaice@gmail.com
+git config --global init.defaultBranch "main"
