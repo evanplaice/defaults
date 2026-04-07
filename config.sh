2729d0604678f5395b0e4513c09e5ae6f5f49997
Evan Plaice <evanplaice@gmail.com>
2026-04-06 18:51:01 -0600

    Add Git Config

---
+++ config.sh
@@ -0,0 +1,6 @@
+# Config git
+
+git config --global user.name "Evan Plaice"
+git config --global user.email "evanplaice@gmail.gom"
+git config --global commit.gpgSign true
+git config --global user.signingkey evanplaice@gmail.com
