cfa5c7f5988f354ed2096ad65145448185579d7e
Evan Plaice <evanplaice@gmail.com>
2026-05-28 21:53:31 -0600

    Add XDG Config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+# Add XDG Config
+mkdir -p ~/{.config,.cache,.local/share,.local/state}
+echo 'export XDG_CONFIG_HOME="$HOME/.config"' >> ~/.zshrc
+echo 'export XDG_CACHE_HOME="$HOME/.cache"' >> ~/.zshrc
+echo 'export XDG_DATA_HOME="$HOME/.local/share"' >> ~/.zshrc
+echo 'export XDG_STATE_HOME="$HOME/.local/state"' >> ~/.zshrc
+cat > ~/Library/LaunchAgents/setenv.XDG_CONFIG.plist << 'EOF'
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>setenv.XDG_CONFIG</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>/bin/launchctl</string>
+        <string>setenv</string>
+        <string>XDG_CONFIG_HOME</string>
+        <string>/Users/evanplaice/.config</string>
+        <string>setenv</string>
+        <string>XDG_CACHE_HOME</string>
+        <string>/Users/evanplaice/.cache</string>
+        <string>setenv</string>
+        <string>XDG_DATA_HOME</string>
+        <string>/Users/evanplaice/.local/share</string>
+        <string>setenv</string>
+        <string>XDG_STATE_HOME</string>
+        <string>/Users/evanplaice/.local/state</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+</dict>
+</plist>
+EOF
+launchctl load ~/Library/LaunchAgents/setenv.XDG_CONFIG.plist
+
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
