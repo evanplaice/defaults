f11fc63ca54ce323e445d080386fe17274a0210f
Evan Plaice <evanplaice@gmail.com>
2026-05-31 08:12:50 -0600

    Add Ghostty

--- bootstrap.sh
+++ bootstrap.sh
@@ -51,6 +51,7 @@ xattr -cr /Applications/Chromium.app
 brew install --cask claude-code
 # brew install --cask discord
 # brew install --cask gitkraken
+brew install --cask ghostty
 # brew install --cask keepassxc # replaced by Strongbox
 # brew install --cask libreoffice
 # brew install --cask openemu
